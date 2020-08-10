9687dfdad3e16ef3221919c3ec1767b05ca92fe6
thanhlongtld <thanhlongtld@gmail.com>
2020-08-10 13:12:07 +0700

    add logo, icon and header css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,15 @@
 import React from "react";
-export  default function Header() {
+import "./../css/Header.css"
+import PersonIcon from '@material-ui/icons/Person';
+import QuestionAnswerIcon from '@material-ui/icons/QuestionAnswer';
+
+export default function Header() {
     return (
+        // BEM
         <div className="header">
-            <h2>I am a header</h2>
+            <PersonIcon/>
+            <img className="header_logo" src="https://1000logos.net/wp-content/uploads/2018/07/tinder-logo.png" alt="tinder-logo"/>
+            <QuestionAnswerIcon/>
         </div>
     )
 }
